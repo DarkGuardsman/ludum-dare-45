02d6f767c6714da509476852f0c93374a7e06dbc
Robert WC Seifert <rseifert.phone@gmail.com>
2019-10-05 21:35:55 -0400

    Created simple map and added player movement

---
+++ GunDropEmpire/Assets/scripts/player_move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+
+	public float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate()
+    {
+        //Get input from user
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        
+        //Calc move
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        
+        //Apply move force
+        rb2d.AddForce (movement * speed);
+    }
+}
